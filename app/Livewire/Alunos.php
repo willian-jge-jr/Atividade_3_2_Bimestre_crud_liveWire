39a00e5e4f3ad9b720509704abe13f10a9cefa1f
Willian Junior <willian.jge.jr@gmail.com>
2025-10-31 21:23:02 -0400

    Ajuste blade

--- app/Livewire/Alunos.php
+++ app/Livewire/Alunos.php
@@ -6,8 +6,22 @@ use Livewire\Component;
 
 class Alunos extends Component
 {
+
+    public $nome;
+    public $email;
+    public $telefone;
+    public $nascimento;
+    public $curso;
+
+
     public function render()
     {
         return view('livewire.alunos')->layout('layouts.app');
     }
+
+    public function store() {}
+
+    public function update() {}
+
+    public function delete() {}
 }
